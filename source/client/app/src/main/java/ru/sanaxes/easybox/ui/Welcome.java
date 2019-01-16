fc8a034afffb5d881f27263ebeadbe1966cb4358
Alexander <sanaxes@gmail.com>
2019-01-16 14:59:50 +0500

    git init front-end

---
+++ source/client/app/src/main/java/ru/sanaxes/easybox/ui/Welcome.java
@@ -0,0 +1,14 @@
+package ru.sanaxes.easybox.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import ru.sanaxes.easybox.R;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
